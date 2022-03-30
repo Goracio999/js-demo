ae28a6c038111a123f203e0d541c926e832970b8
Goracio999 <gora999@gmail.com>
2022-03-30 20:51:55 +0300

    changing element by JS
     greeting sensitive by hour

--- styler.js
+++ styler.js
@@ -9,8 +9,6 @@ const colors = [
     'olive',
     'aqua',
     'aquamarine',
-    'azure',
-    'biege',
     'darkcyan',
     'darkolivegreen',
     'skyblue',
@@ -21,8 +19,29 @@ let randomColor = Math.round(Math.random() * (maxColor - minColor) + minColor);
 const backColor = colors[randomColor];
 console.log(backColor);
 document.body.style.backgroundColor = backColor;
-console.log("colors: ", colors.join(", "))
-// changing element       
-        var myFooter = document.getElementById("statusBar");
-        console.log(myFooter);
+console.log("colors: ", colors.join(", "));
+
+// changing element - greeting sensitive by hour       
+var myGreeting = document.getElementById('greeting');
+console.log(myGreeting);
+var currentTime = new Date();
+console.log("Current date: ", currentTime);
+currentHour = currentTime.getHours();
+console.log("Current hour: ", currentHour);
+let greetingStart;
+if (currentHour < 4) {
+    greetingStart = "Добраніч";
+} else if (currentHour < 12) {
+    greetingStart = "Доброго ранку";
+} else if (currentHour < 18) {
+    greetingStart = "Доброго дня";
+} else {
+    greetingStart = "Доброго вечора";
+}
+const greetingMessage = `${greetingStart}, ми з України!`
+console.log(greetingMessage);
+myGreeting.innerHTML = greetingMessage;
+// status bar
+var myFooter = document.getElementById("statusBar");
+console.log(myFooter);
 myFooter.innerHTML = "Background color: " + backColor;
