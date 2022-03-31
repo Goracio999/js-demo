483898b632ccf0af812627124f5fb274ddd57a03
Goracio999 <gora999@gmail.com>
2022-03-31 23:25:06 +0300

    some interactive added

---
+++ dom.js
@@ -0,0 +1,27 @@
+const btnAdd = document.querySelector('button[data-add]');
+const valueInput = document.querySelector('input[data-value]');
+const outputEl = document.querySelector('.js-output span');
+const resetBtn = document.querySelector('button[data-reset]');
+let totalSum = 0;
+console.log(valueInput.value);
+console.log(outputEl);
+btnAdd.addEventListener('click', function () {
+    console.log('clicked');
+    
+    const valAdd = Number(valueInput.value);
+    totalSum += valAdd;
+    console.log(valAdd, typeof valAdd);
+    valueInput.value = '';
+    console.log("total : ", totalSum);
+    outputEl.textContent = totalSum;
+    if (totalSum < 0) {
+      // change color to "red" for negative value  
+    } else {
+      // set color to "blue" for not negative value
+    }
+});
+
+resetBtn.addEventListener('click', function () {
+    totalSum = 0;
+    outputEl.textContent = totalSum;
+});
