a9c4f1e20e85be2c6fbb3bd7c3e7b54e78c7e99e
Goracio999 <gora999@gmail.com>
2022-04-02 23:57:26 +0300

    strings operations
    Some string operations added

---
+++ strings.js
@@ -0,0 +1,22 @@
+// Operation with strings
+function slugify(title) {
+    // Change code below this line
+    const normalString = title.toLowerCase();
+    console.log(normalString);
+    const arrayFromTitle = normalString.split(" ");
+    console.log(arrayFromTitle)
+    const slugString = arrayFromTitle.join("-");
+    console.log(slugString);
+    return slugString;
+}
+
+ const words = ["JavaScript", "це", "цікаво"];
+ console.log(words.join(" ")); // "JavaScript це цікаво"
+ console.log(words.join("_")); // "JavaScript-це-цікаво"
+ 
+console.log(slugify("Arrays for begginers"));
+console.log(slugify("English for developer"));
+console.log(slugify("Ten secrets of JavaScript"));
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS"));
+console.log(slugify("putin must DIE"));
+console.log(slugify(words.join(" ")))
